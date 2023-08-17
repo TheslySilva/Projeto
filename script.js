750c13bb93d178926f3175343086af958ac16d87
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-08-17 10:39:24 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const pessoa={
-	nome: "thesly",
-	idade: 21,
-	cidade: "Pimenteiras"
-}
+const pessoa = {
+  "nome": "thesly",
+  idade: 21,
+  cidade: "Pimenteiras",
+};
 
-document.getElementById("nome").textContent= pessoa.nome
+document.getElementById("nome").textContent = pessoa.nome;
