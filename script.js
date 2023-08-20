55a67d147a9dca5c07941f5cd4a758641cfc9b8f
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-08-20 09:20:51 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const agora = new Date();
 const horario = agora.getHours();
-console.logo(horario)
-if (hora >= 6 && hora <= 11) {
+console.log(horario);
+if (hora >= 9 && hora <= 11) {
   document.getElementById("hora").innerHTML = "Bom Dia!";
 } else if (hora >= 12 && hora <= 17) {
   document.getElementById("hora").innerHTML = "Boa Tarde!";
