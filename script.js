cd1f054711b90bd2071d15701380165583d9fd57
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-08-20 12:53:11 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const agora = new Date();
-const horario = agora.getHours();
-console.log(horario);
-if (horario >= 9 && hora <= 11) {
+const hora = agora.getHours();
+
+if (hora >= 6 && hora <= 11) {
   document.getElementById("hora").innerHTML = "Bom Dia!";
-} else if (horario >= 12 && hora <= 17) {
+} else if (hora >= 12 && hora <= 17) {
   document.getElementById("hora").innerHTML = "Boa Tarde!";
-} else if (horario >= 18 && hora <= 23) {
+} else if (hora >= 18 && hora <= 23) {
   document.getElementById("hora").innerHTML = "Boa Noite!";
 } else {
   document.getElementById("hora").innerHTML = "Boa Madrugada!";
