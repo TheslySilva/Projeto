29e3eb4b0242983be06528a6852845751cbf1895
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-08-20 02:09:24 -0300

    update script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const agora = new Date();
+const horario = 15;
+
+if (hora >= 6 && hora <= 11) {
+  document.getElementById("hora").innerHTML = "Bom Dia!";
+} else if (hora >= 12 && hora <= 17) {
+  document.getElementById("hora").innerHTML = "Boa Tarde!";
+} else if (hora >= 18 && hora <= 23) {
+  document.getElementById("hora").innerHTML = "Boa Noite!";
+} else {
+  document.getElementById("hora").innerHTML = "Boa Madrugada!";
+}
